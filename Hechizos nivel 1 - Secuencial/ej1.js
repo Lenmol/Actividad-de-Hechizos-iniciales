911396de7160138f2039ed91829afa9db3e75f4e
pc 0 <>
2024-04-09 11:58:20 -0300

    Actividad completada

--- Hechizos nivel 1 - Secuencial/ej1.js
+++ Hechizos nivel 1 - Secuencial/ej1.js
@@ -8,7 +8,7 @@ const leer = require("prompt-sync")();
  * Solicita la cantidad de escarabajos de ojo de tigre y el número de raíces de mandrágora necesarios para hacer una poción, y luego calcula la cantidad total de ingredientes necesarios
  * 
  * Por ejemplo si es necesario 8 raíces de mandrágora y 5 escarabajos de ojo de tigre para hacer la poción entonces el total de ingredientes es 13
- */
+ */ 
 
 console.log("Se necesita 8 raíces de mandrágora y 5 escarabajos de ojo de tigre para hacer la poción")
 const ESCARABAJOS_OJOS_TIGRE = 8;
