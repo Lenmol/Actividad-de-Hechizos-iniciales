bbd80340aeb8bbaef4c9107492691f9742131538
Lenmol <leonel.md6898@gmail.com>
2024-04-11 13:15:52 +0200

    Actividad completada

--- Hechizos nivel 2 - Selectivo/if - else if - else/ej4.js
+++ Hechizos nivel 2 - Selectivo/if - else if - else/ej4.js
@@ -8,11 +8,25 @@ const leer = require("prompt-sync")();
  * 
  * Por ejemplo si la astucia es 90 y la lealtad es 40 entonces la varita es apta, en otro caso si la astucia es 30 y la lealtad es 60 la varita no es apta
  */
+const NIVEL_ACEPTADO_LEALTAD = 70;
+const NIVEL_ACEPTADO_ASTUCIA = 80;
 
+let nivelLealtadIngresado= 0;
+let nivelAstuciaIngresado = 0;
 
-function main() {
+console.log("Ingrese el nivel de lealtad de su varita");
+nivelLealtadIngresado = Number(leer());
+console.log("Ingrese la astucia de su varita");
+nivelAstuciaIngresado = Number(leer());
+
+function verificacionVarita(nivelLealtadIngresado, nivelAstuciaIngresado) {
+    if ((nivelLealtadIngresado >= NIVEL_ACEPTADO_LEALTAD )||(nivelAstuciaIngresado >= NIVEL_ACEPTADO_ASTUCIA)){
+        return console.log("La varita es apta para su uso")
+    } else {
+        return console.log("La varita no es apta para su uso")
+    }
     
 }
 
 
-main();
+verificacionVarita(nivelLealtadIngresado, nivelAstuciaIngresado);
