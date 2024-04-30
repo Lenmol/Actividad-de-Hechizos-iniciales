7d1baa46e346c307e6fbf47f9e94cb935526502a
pc 0 <>
2024-04-30 12:12:00 -0300

    variables y un while

--- Hechizos nivel 3 - Iterativo/2 - while/ej2.js
+++ Hechizos nivel 3 - Iterativo/2 - while/ej2.js
@@ -16,9 +16,46 @@ Recuerda, tanto tú como tu ser querido pueden recibir ataques de los Dementores
  */
 
 
+// tu misión es defender tu propia felicidad y los recuerdos alegres al igual que de una persona querida contra los Dementores. 
+
+
+// Pero ten cuidado, estos seres malignos no dudarán en atacarte y tratar de absorber tu felicidad y la de tu ser querido.
+
+const ATAQUE_DEMENTOR = 15;
+
 function main() {
+
+    let felicidadUser = 100; 
+    let felicidadAmigo = 100;
+    let numAleatorio = Math.floor(Math.random()*4)
+    let numIngresado = 0;
+    let perdioDementor = true;
+    let perdioUser = true;
+    let perdioAmigo = true;
+
+    console.log("ingresar números aleatorios entre 0 y 3. Si adivinas correctamente el número aleatorio generado por los Dementores, lograrás defenderte y proteger tus recuerdos.");
+    numIngresado = Number(leer());
+
+
+    if ((perdioDementor && perdioUser && perdioAmigo) == true){
+        
+        if(numAleatorio == numIngresado){
+            
+        }
+    }
     
+
 }
+
+
+    // si los Dementores lanzan más de cinco ataques, existe la posibilidad de que aparezca una persona misteriosa para ayudarte a derrotar a los Dementores
+
+
+    // anto tú como tu ser querido pueden recibir ataques de los Dementores, pero solo uno a la vez, a menos que ocurra un número especial en el controlador de ataque. 
+    
+    // Si tu felicidad y recuerdos alegres llegan a cero o la de tu ser querido entonces es el fin para ambos. Mantén tu concentración y tu valentía mientras te enfrentas a este desafío.
+
+
 
 
 main();
