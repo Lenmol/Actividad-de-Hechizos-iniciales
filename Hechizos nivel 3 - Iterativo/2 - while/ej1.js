3f994374ffa3a7e985ec8bd9b0ed067a1738de27
Lenmol <leonel.md6898@gmail.com>
2024-05-03 08:05:31 +0200

    cambios

--- Hechizos nivel 3 - Iterativo/2 - while/ej1.js
+++ Hechizos nivel 3 - Iterativo/2 - while/ej1.js
@@ -18,12 +18,73 @@ Vidas iniciales/minimas, daño jugador/enemigo y hechizos a usar (minimo 3) qued
 Considerar que:
 - la vida del enemigo no puede ser menor que la del jugador
 - cada hechizo tiene un valor de daño unico y el enemigo tiene 1 solo hechizo de ataque
- */
+*/
+
+
+const VIDA_MIN_USER = 1;
+
+const NUMERO_HECIZO_1 = 3;
+const DAÑO_HECHIZO_1 = 40;
+
+const NUMERO_HECIZO_2 = 7;
+const DAÑO_HECHIZO_2 = 60;
+
+const NUMERO_HECIZO_3 = 1;
+const DAÑO_HECHIZO_3 = 30;
+
+const VIDA_MIN_VOLDEMORT = -1;
+const ATAQUE_VOLDEMORT =50;
 
 
 function main() {
+    let voldemorVivo = true;
+    let jugadorVivo = true;
+    let perdedor = false;
+    let hechizoIngresado = 0;
+    let numeroAleatorio = Math.floor((Math.random()*10) + 1);
+    let vidaUser = 200;
+    let vidaVoldemort = 300;
+
+    console.log("Cuidado ahì viene Voldemort, detenlo!!!");
+    console.log("Los hechizos lanzados tendran una porbabilidad de acertar o no");
+    console.log("Tendras tres hehizos Hechizo-1[3], Hechizo-2[7], Hechizo-3[1]");
+    hechizoIngresado = Number(leer());
+    
+    
+    while((voldemorVivo && jugadorVivo) == true){
+        
+        atacarVoldemort(hechizoIngresado, NUMERO_HECIZO_1, DAÑO_HECHIZO_1, numeroAleatorio, vidaVoldemort);
+
+        ataqueRecibido(hechizoIngresado, NUMERO_HECIZO_1, vidaUser);
+        break;
+        
+
+    }
     
 }
 
 
 main();
+
+function atacarVoldemort(numIngresado, numeroHechizo, daño, numAleatorio, Voldemort){
+
+    if (numIngresado == numeroHechizo){
+        console.log("[ELEGISTE EL HECHIZO-1]");
+        if (numIngresado == numAleatorio){
+            Voldemort -= daño;
+            console.log(`[ATAQUE EXITOSO, VOLDEMORT TIENE ${Voldemort}]`);
+        }
+    }
+    return Voldemort;
+
+}
+
+function ataqueRecibido(numIngresado, numeroHechizo, vidaJugador){
+    if(numIngresado != numeroHechizo){
+        console.log("[FALLASTE!!!]");
+        console.log(`Voldemort te va a atacar`);
+        vidaJugador -= ATAQUE_VOLDEMORT;
+        console.log(`[ATAQUE RECIBIDO, VIDA JUGADOR ${vidaJugador}]`);
+    }
+    return vidaJugador;
+}
